8a31d5579cedcdd903b18f0c322562f62fe1088d
royteutsch <teutschroy@gmail.com>
2021-11-04 12:26:59 +0200

    pain

--- database_locker.py
+++ database_locker.py
@@ -2,10 +2,27 @@ import os
 import time
 import pickle
 from typing import Optional
-
 import database_writer
 import threading
 from threading import Semaphore
+import multiprocessing
+from multiprocessing import Semaphore as SemaphoreProcess
+from multiprocessing import Lock as LockProcess
+
+
+class MyLock(LockProcess):  # Literally just a lock with the ability to check if its locked
+    
+    def __init__(self):
+        super(MyLock, self).__init__()
+    
+    def locked(self):
+        is_locked = super(MyLock, self).acquire(block=False)
+        if not is_locked:
+            return True
+        else:
+            super(MyLock, self).release()
+            return False
+
 
 class MySemaphore(Semaphore):
     def __init__(self, maxcount):
@@ -25,13 +42,37 @@ class MySemaphore(Semaphore):
         return self.counter == self.maxcount
 
 
+class MySemaphoreProcess(SemaphoreProcess):
+    def __init__(self, maxcount):
+        self.counter = 0
+        self.maxcount = maxcount
+        super(MySemaphoreProcess, self).__init__(maxcount)
+
+    def acquire(self):
+        super(MySemaphoreProcess, self).acquire()
+        self.counter += 1
+
+    def release(self) -> None:
+        self.counter -= 1
+        super(MySemaphoreProcess, self).release()
+
+    def isFullyLocked(self):
+        return self.counter == self.maxcount
+
+
 class DatabaseLocker(database_writer.DatabaseWriter):
     _rLock_sema = MySemaphore(10)
+    _rLock_semaProcess = MySemaphoreProcess(10)
 
-    def __init__(self, file_location: str):
+    def __init__(self, file_location: str, mode: str):
         super().__init__(file_location)
-        self.wLock = threading.Lock()
-        self.aquireLock = threading.Lock()
+        if mode == "T":  # Thread mode
+            self.wLock = threading.Lock()
+            self.aquireLock = threading.Lock()
+        else:  # Process mode
+            self.wLock = MyLock()
+            self.aquireLock = multiprocessing.Lock()
+        self.number = 0
         if os.path.getsize(self.file_loc) != 0:  # This means that the file is not empty
             db_file = open(self.file_loc, 'rb')
             self.data = pickle.load(db_file)
@@ -43,10 +84,9 @@ class DatabaseLocker(database_writer.DatabaseWriter):
             while DatabaseLocker._rLock_sema.counter > 0:
                 time.sleep(0.0001)
         try:
-            DatabaseLocker.current_mode = "W"
             super().set_value(key, val)
         finally:
-            DatabaseLocker.current_mode = "N"
+            self.number += 10
             self.wLock.release()
 
     def get_value(self, key):  # Reading Privilege
@@ -55,10 +95,9 @@ class DatabaseLocker(database_writer.DatabaseWriter):
             while self.wLock.locked():
                 time.sleep(0.0001)
         try:
-            DatabaseLocker.current_mode = "R"
             return super().get_value(key)
         finally:
-            DatabaseLocker.current_mode = "N"
+            self.number -= 10
             DatabaseLocker._rLock_sema.release()
 
     def delete_value(self, key):  # Writing Privilege
@@ -67,10 +106,9 @@ class DatabaseLocker(database_writer.DatabaseWriter):
             while DatabaseLocker._rLock_sema.counter > 0:
                 time.sleep(0.0001)
         try:
-            DatabaseLocker.current_mode = "W"
             val = super().delete_value(key)
             return val
         finally:
-            DatabaseLocker.current_mode = "N"
+            self.number += 10
             self.wLock.release()
 
--- testtestestesetestest.py
+++ testtestestesetestest.py
@@ -1,34 +1,36 @@
 import random
 
 import database_locker
-import threading
+import threading as parallel
 import time
 
 
-def read_worker(key, num):
+def read_worker(key):
     for i in range(1000):
-        print(d.get_value(key=key))
-        print(num)
+        val = d.get_value(key=key)
+        print(key + " corresponds to " + val)
 
 
 def write_worker(key):
-    for i in range(1000):
+    for i in range(10000):
         d.set_value(key=key, val=str(i))
-        print("Wrote " + str(i) +" to key " + key + " at time " + str(time.time()))
+        print("Wrote " + str(i) + " to key " + key + " at time " + str(time.time()))
+        print("Number: " + str(d.number))
 
-def worker(key):
+
+def worker():
     time.sleep(random.random())
 
-d = database_locker.DatabaseLocker("dbText.txt")
+
+d = database_locker.DatabaseLocker("dbText.txt", "T")
 threads_1 = []
-for index in range(3):
-    x = threading.Thread(target=write_worker, args=('hello', ))
+for index in range(10):
+    x = parallel.Thread(target=write_worker, args=('hello',))
     threads_1.append(x)
 
-
 threads_2 = []
-for index in range(10):
-    x = threading.Thread(target=read_worker, args=('hello', index, ))
+for index in range(100):
+    x = parallel.Thread(target=read_worker, args=('hello', ))
     threads_2.append(x)
 for t in threads_1:
     t.start()
@@ -36,3 +38,4 @@ for t in threads_2:
     t.start()
 
 print(d.data)
+print(d.number)
