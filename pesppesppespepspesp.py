7e4e52bb882d5e6b7c23e25e2d00696a67430a7f
royteutsch <teutschroy@gmail.com>
2021-11-04 12:27:38 +0200

    my ass

---
+++ pesppesppespepspesp.py
@@ -0,0 +1,41 @@
+import random
+
+import database_locker
+import multiprocessing as parallel
+import time
+
+
+def read_worker(key):
+    for i in range(1000):
+        val = d.get_value(key=key)
+        print(key + " corresponds to " + val)
+
+
+def write_worker(key):
+    for i in range(10000):
+        d.set_value(key=key, val=str(i))
+        print("Wrote " + str(i) + " to key " + key + " at time " + str(time.time()))
+        print("Number: " + str(d.number))
+
+
+def worker():
+    time.sleep(random.random())
+
+
+d = database_locker.DatabaseLocker("dbText.txt", "T")
+threads_1 = []
+for index in range(10):
+    x = parallel.Process(target=write_worker, args=('hello',))
+    threads_1.append(x)
+
+threads_2 = []
+for index in range(100):
+    x = parallel.Process(target=read_worker, args=('hello', ))
+    threads_2.append(x)
+for t in threads_1:
+    t.start()
+for t in threads_2:
+    t.start()
+
+print(d.data)
+print(d.number)
